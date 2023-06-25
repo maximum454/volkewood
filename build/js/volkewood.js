5b9042c514815bca975032b1483a456ed4cf11ba
Maxim Karachyov <karachyov.m@gmail.com>
2023-06-25 17:22:40 +0600

    fix

--- build/js/volkewood.js
+++ build/js/volkewood.js
@@ -1435,9 +1435,34 @@ function getDirection() {
 
 
 $(function (){
+
+    $('.js-menu').on('click', function (){
+        $('.nav').addClass('active')
+        $('body').addClass('lock')
+        $('.wrp__overlay').addClass('active')
+    })
+
+    $('.wrp__overlay').on('click', function (){
+        $('.nav').removeClass('active')
+        $('body').removeClass('lock')
+        $('.wrp__overlay').removeClass('active')
+    })
+
     $('.tabs__caption').on('click', 'li:not(.active)', function () {
         $(this)
             .addClass('active').siblings().removeClass('active')
             .closest('.tabs').children('.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+    });
+
+    $(window).scroll(function() {
+        if($(this).scrollTop() != 0) {
+            $('#to-top').fadeIn();
+        } else {
+            $('#to-top').fadeOut();
+        }
+    });
+
+    $('#to-top').click(function() {
+        $('html, body').animate({scrollTop:0}, '300');
     });
 })
