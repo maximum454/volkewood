762654124570663aaf394c7658455a9ecd194113
Maxim Karachyov <karachyov.m@gmail.com>
2023-06-25 16:54:31 +0600

    tabs

--- build/js/volkewood.js
+++ build/js/volkewood.js
@@ -1435,5 +1435,9 @@ function getDirection() {
 
 
 $(function (){
-
+    $('.tabs__caption').on('click', 'li:not(.active)', function () {
+        $(this)
+            .addClass('active').siblings().removeClass('active')
+            .closest('.tabs').children('.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+    });
 })
